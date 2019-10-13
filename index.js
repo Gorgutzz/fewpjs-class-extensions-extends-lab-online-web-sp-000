a630f4a875e34dcce697e887656a2bd3d110cc1c
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-13 20:29:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ class Polygon {
 
 class Triangle extends Polygon {
     get isValid() {
-      let side1 = this.sides[0]
-      let side2 = this.sides[1]
-      let side3 = this.sides[2]
-      return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+      if (this.countSides == 3) {
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+        return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+      }
     }
+     else {
+     return false;
+   }
 }
